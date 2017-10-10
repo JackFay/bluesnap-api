5ef24ceb598a1a15ce723d2dcaacc618c2f93d64
Jack Fay <jrfay08@gmail.com>
2017-10-10 00:58:02 -0500

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,46 @@
+import { version } from '../../package.json';
+import { Router } from 'express';
+import facets from './facets';
+import axios from "axios";
+
+export default ({ config, db }) => {
+	let api = Router();
+
+	// mount the facets resource
+	api.use('/facets', facets({ config, db }));
+
+	api.post('/', (req, res) => {
+		var http = require("https");
+
+		var options = {
+		  "method": "POST",
+		  "hostname": "sandbox.bluesnap.com",
+		  "port": null,
+		  "path": "/services/2/batch-transactions",
+		  "headers": {
+		    "content-type": "application/xml",
+		    "authorization": req.body.api,
+		    "cache-control": "no-cache",
+		    "postman-token": "d0f3b0aa-cff2-70ad-554f-11321617f822"
+		  }
+		};
+
+		var bsReq = http.request(options, function (bsRes) {
+		  var chunks = [];
+
+		  bsRes.on("data", function (chunk) {
+		    chunks.push(chunk);
+		  });
+
+		  bsRes.on("end", function () {
+		    var body = Buffer.concat(chunks);
+		    res.send(body.toString());
+		  });
+		});
+
+		bsReq.write(req.body.xml);
+		bsReq.end();
+	});
+
+	return api;
+}
---
+++ src/index.js
@@ -0,0 +1,50 @@
+import http from 'http';
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import initializeDb from './db';
+import middleware from './middleware';
+import api from './api';
+import config from './config.json';
+
+let app = express();
+app.server = http.createServer(app);
+
+// logger
+app.use(morgan('dev'));
+
+// 3rd party middleware
+app.use(cors({
+	exposedHeaders: config.corsHeaders
+}));
+
+app.use(bodyParser.json({
+	limit : config.bodyLimit
+}));
+
+// connect to db
+initializeDb( db => {
+
+	// internal middleware
+	app.use(middleware({ config, db }));
+
+	// api router
+	app.use('/api', api({ config, db }));
+
+	app.server.listen(process.env.PORT || config.port, () => {
+		console.log(`Started on port ${app.server.address().port}`);
+	});
+
+	var cors_proxy = require('cors-anywhere');
+	cors_proxy.createServer({
+	    originWhitelist: [], // Allow all origins
+	    requireHeader: ['origin', 'x-requested-with'],
+	    removeHeaders: ['cookie', 'cookie2']
+	}).listen(8081, 'localhost', function() {
+	    console.log('Running CORS Anywhere');
+	});
+
+});
+
+export default app;
