94a943e83bef4bfbdf91de4e86e1b708eb6d004a
Ubuntu <ubuntu@ip-172-31-23-24.us-west-2.compute.internal>
2017-10-20 13:55:59 +0000

    removed cors anywhere

--- src/index.js
+++ src/index.js
@@ -35,15 +35,6 @@ initializeDb( db => {
 		console.log(`Started on port ${app.server.address().port}`);
 	});
 
-	var cors_proxy = require('cors-anywhere');
-	cors_proxy.createServer({
-	    originWhitelist: [], // Allow all origins
-	    requireHeader: ['origin', 'x-requested-with'],
-	    removeHeaders: ['cookie', 'cookie2']
-	}).listen(8081, 'localhost', function() {
-	    console.log('Running CORS Anywhere');
-	});
-
 });
 
 export default app;
