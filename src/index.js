6003139e26a1cd3bee3d64b2e1a67aee99dfbfbd
Jack Fay <jrfay08@gmail.com>
2017-10-24 18:22:16 -0500

    update

--- src/index.js
+++ src/index.js
@@ -22,10 +22,6 @@ app.use(cors({
 
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
-app.use(express.static(path.join(__dirname, './dist/')));
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/build/index.html'))
-})
 
 // connect to db
 initializeDb( db => {
