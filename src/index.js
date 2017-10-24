22ab1a1d58ef9fdb5bf5eed3c49f77f57474b92b
Jack Fay <jack.fay@cerner.com>
2017-10-24 16:36:21 -0500

    server setup stuff

--- src/index.js
+++ src/index.js
@@ -22,9 +22,9 @@ app.use(cors({
 
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
-
+app.use(express.static(path.join(__dirname, '/../dist/')));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/build/index.html'))
+    res.sendFile(path.join(__dirname, '/build/index.html'))
 })
 
 // connect to db
